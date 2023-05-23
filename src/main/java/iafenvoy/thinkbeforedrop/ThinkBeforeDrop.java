fd9455ae52bb3b33e17e71963db54fa8dc95d982
awesomebobby <99313339+awesomebobby@users.noreply.github.com>
2023-05-24 07:47:20 +0800

    Update ThinkBeforeDrop.java

--- src/main/java/iafenvoy/thinkbeforedrop/ThinkBeforeDrop.java
+++ src/main/java/iafenvoy/thinkbeforedrop/ThinkBeforeDrop.java
@@ -1,13 +1,18 @@
 package iafenvoy.thinkbeforedrop;
 
-import net.fabricmc.api.ModInitializer;
+import iafenvoy.thinkbeforedrop.ThinkBeforeDrop;
+import iafenvoy.thinkbeforedrop.config.Configs;
+import iafenvoy.thinkbeforedrop.registry.KeyBind;
+import fi.dy.masa.malilib.config.ConfigManager;
+import net.fabricmc.api.ClientModInitializer;
 
-public class ThinkBeforeDrop implements ModInitializer {
+public class ThinkBeforeDrop implements ClientModInitializer {
     public static final String MOD_ID = "thinkbeforedrop";
     public static final String MOD_NAME = "Think Before Drop";
 
     @Override
-    public void onInitialize() {
-
+    public void onInitializeClient() {
+        KeyBind.register();
+        ConfigManager.getInstance().registerConfigHandler(MOD_ID, Configs.INSTANCE);
     }
 }
